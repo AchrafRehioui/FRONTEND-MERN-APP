59dbb4106b9e9d46f21487cb988cd1e9952a7337
unknown <achraf.rehioui.1@gmail.com>
2020-05-06 16:09:00 +0200

    Adding a UserItem Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import UserList from '../components/UsersList';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
     const USERS = [
@@ -9,7 +9,8 @@ const Users = () => {
             name: 'sa ach',
             image: 'https://images.pexels.com/photos/2397645/pexels-photo-2397645.jpeg?cs=srgb&dl=vue-plongeante-paysage-montagnes-nature-2397645.jpg&fm=jpg',
             places: 3
-        }];
+        }
+    ];
 
     return <UsersList items={USERS} />;
 };
