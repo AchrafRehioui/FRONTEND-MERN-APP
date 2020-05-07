b64bd5a93c1280667c4f0778bd66e5d91103a318
unknown <achraf.rehioui.1@gmail.com>
2020-05-07 19:28:49 +0200

    Adding NavLinks

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -9,8 +9,7 @@ const UsersList = props => {
             <div className="center">
                 <Card>
                     <h2>No users found.</h2>
-                </Card>
-                
+                </Card> 
             </div>
         );
     }
